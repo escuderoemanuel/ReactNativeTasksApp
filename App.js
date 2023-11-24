dd9e034c7b0035cacb29a51993f5b1a884227035
Emanuel Escudero <escudero.mdz@gmail.com>
2023-11-24 00:07:09 -0300

    Iniciando Repo

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleApp}>Task App</Text>
+      <View style={styles.inputContainer} >
+        <TextInput style={styles.textInput} placeholder='Ingresar Tarea'/>
+        <Button title='Add'/>
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  titleApp: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#489'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    width: 200,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 5,
+  }
 });
