ec54c0563785e5a05e4ca87795a2bfb44e62c114
Emanuel Escudero <escudero.mdz@gmail.com>
2023-11-28 01:15:09 -0300

    Added Styles to textItem & to itemList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View, Text, FlatList } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Text, FlatList, StatusBar, Pressable } from 'react-native';
 
 export default function App() {
   const [textItem, setTextItem] = useState('')
@@ -12,39 +12,54 @@ export default function App() {
 
   const addItemToList = () => {
     setItemList(prevState => [...prevState, { id: Math.random().toString(), value: textItem }])
+    //console.log(itemList)
     setTextItem('')
   }
 
-  const renderListItem = ({ item }) => {
+  const renderListItem = ({ item }) => (
     <View style={styles.itemList}>
-      <Text>{item.value}</Text>
-      <Button title='x' />
+      <Text style={styles.itemText}>{item.value}</Text>
+      {/* <Button 
+        title=' x '
+        color='#212121'
+        accessibilityLabel='Button to delete Task of the List'
+        /> */}
+      <Pressable
+        style={styles.buttonDelete}
+        accessibilityLabel='Button to delete Task of the List'
+      >
+        <Text style={styles.textButtonDelete}>×</Text>
+      </Pressable>
     </View>
-  }
+  )
 
   return (
     <>
-    <View style={styles.container}>
-      <Text style={styles.titleApp}>Task App</Text>
-      <View style={styles.inputContainer} >
-        <TextInput 
-          style={styles.textInput}
-          placeholder='Ingresar Tarea'
-          onChangeText={onChangeTextHandler}
-          value={textItem}
+      <View style={styles.container}>
+        <StatusBar />
+        <Text style={styles.titleApp}>✓  MobileTask</Text>
+        <View style={styles.inputContainer} >
+          <TextInput
+            style={styles.textInput}
+            placeholder='Ingresar Tarea'
+            placeholderTextColor='whitesmoke'
+            onChangeText={onChangeTextHandler}
+            value={textItem}
           />
-        <Button 
-          title='Add'
-          color='gray'
-          onPress={addItemToList}
+          <Button
+            title='  +  '
+            color='rgb(245, 73, 144)'
+            accessibilityLabel='Button to add a Task to the List'
+            onPress={addItemToList}
           />
+        </View>
+        <FlatList
+          style={styles.flatList}
+          data={itemList}
+          renderItem={renderListItem}
+          keyExtractor={item => item.id}
+        />
       </View>
-      <FlatList
-        data={itemList}
-        renderItem={renderListItem}
-        keyExtractor={item => item.id}
-      />
-    </View>
     </>
   );
 }
@@ -52,26 +67,62 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
+    color: 'whitesmoke',
     alignItems: 'center',
     justifyContent: 'space-around',
   },
   titleApp: {
-    fontSize: 25,
+    fontSize: 30,
     fontWeight: 'bold',
-    color: '#489'
+    fontStyle: 'italic',
+    color: 'whitesmoke',
+    marginBottom: 30,
+    padding: 10,
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    width: '90%'
   },
   textInput: {
-    width: 200,
-    borderColor: '#ccc',
-    borderWidth: 1,
+    width: '85%',
     padding: 5,
+    borderBottomColor: "rgb(245, 73, 144)",
+    borderBottomWidth: 2,
+    color: 'whitesmoke',
+    marginRight: 5,
+    fontSize: 20
+  },
+  flatList: {
+    width: '90%',
+    marginTop: 30
+  },
+  itemList: {
+    justifyContent: 'center',
+    color: 'whitesmoke',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "rgb(245, 73, 144)",
+    borderRadius: 10,
+  },
+  itemText: {
+    color: 'whitesmoke',
+    fontSize: 18
+  },
+  buttonDelete: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'whitesmoke'
+  },
+  textButtonDelete: {
+    color: 'whitesmoke',
+    fontSize: 25
   }
 });
